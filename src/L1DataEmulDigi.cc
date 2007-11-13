79e3ae082341516790306283b8bd4cdee488e178
cmsbuild <>
2007-11-13 17:27:28 +0000

    This commit was manufactured by cvs2git to create tag 'DataFormats- L1Trigger-V02-05-04'.
    Sprout from master 2007-11-13 17:27:23 UTC Werner Man-Li Sun <sha1-c4a98c2838358beb3b4b0003c9a7fda3e700bfd6@cern.ch> 'Switch to covariant return type for clone().'
    Cherrypick from master 2007-10-31 20:36:08 UTC Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch> 'format containers for data|emulator monitoring':
        interface/L1DataEmulDigi.h
        src/L1DataEmulDigi.cc
        src/L1DataEmulRecord.cc

--- src/L1DataEmulDigi.cc
+++ src/L1DataEmulDigi.cc
@@ -1,36 +1,21 @@
 #include "DataFormats/L1Trigger/interface/L1DataEmulDigi.h"
 #include <iomanip>
 
-bool L1DataEmulDigi::empty() const {
-  if(m_sid == m_null  || m_cid == m_null ) 
-    return true;
-  bool val = true;
-  for(int i=0; i<2; i++) 
-    val &= ( m_location[i]==m_null );
-  return val;
-}
-
-int L1DataEmulDigi::reset() {
-  m_null = -99;
-  m_sid = m_null; 
-  m_cid = m_null;
-  for(int i=0; i<3; i++) 
-    m_location[i]=m_null;
-  m_type = m_null; 
-  std::fill(m_data,m_data+sizeof(m_data)/sizeof(m_data[0]),0);
-  std::fill(m_rank,m_rank+sizeof(m_rank)/sizeof(m_rank[0]),m_null);
-  return m_null;
-}
-
 L1DataEmulDigi::L1DataEmulDigi() {
-  reset();
+  m_sid = -1; m_cid = -1;
+  m_location[0]=99; m_location[1]=99; m_location[2]=99;
+  m_type = -1; 
+  std::fill(m_data,m_data+sizeof(m_data)/sizeof(m_data[0]),0);
+  std::fill(m_rank,m_rank+sizeof(m_rank)/sizeof(m_rank[0]),0.);
 }
 
-L1DataEmulDigi::L1DataEmulDigi( int sid, int cid, double x1, double x2, double x3, int n) {
-  reset();
+L1DataEmulDigi::L1DataEmulDigi( int sid, int cid, double x1, double x2, double x3, int n)
+{
   m_sid = sid; m_cid = cid;
   m_location[0]=x1; m_location[1]=x2; m_location[2]=x3;
   m_type = n; 
+  std::fill(m_data,m_data+sizeof(m_data)/sizeof(m_data[0]),0);
+  std::fill(m_rank,m_rank+sizeof(m_rank)/sizeof(m_rank[0]),0.);
 }
 
 L1DataEmulDigi::~L1DataEmulDigi() {}
@@ -44,17 +29,17 @@ std::ostream& operator<<(std::ostream& s, const L1DataEmulDigi& de) {
       << " subsystem: "  << std::setw(2) << de.sid()
       << " (cid."        << std::setw(2) << de.cid() << ")"
       << " location: " 
-      << "(" << std::setw(5) << std::setprecision(2) << de.x1() 
-      << "," << std::setw(5) << std::setprecision(2) << de.x2() 
-      << "," << std::setw(5) << std::setprecision(2) << de.x3() << ")"
+      << "(" << std::setw(3) << std::setprecision(4) << de.x1() 
+      << "," << std::setw(3) << std::setprecision(4) << de.x2() 
+      << "," << std::setw(3) << std::setprecision(4) << de.x3() << ")"
       << " type: " << de.type()
-      << std::hex << std::setfill('0')
+      << std::hex
       << " dword:0x" << std::setw(8)<< word[0]
       << " eword:0x" << std::setw(8)<< word[1]
-      << std::dec << std::setfill(' ')
+      << std::dec
       << " rank:"
-      << "(" << std::setw(5) << std::setprecision(2) << rankarr[0] 
-      << "," << std::setw(5) << std::setprecision(2) << rankarr[1] << ")";
+      << "(" << std::setw(3) << std::setprecision(4) << rankarr[0] 
+      << "," << std::setw(3) << std::setprecision(4) << rankarr[1] << ")";
     return s;
 }
 
